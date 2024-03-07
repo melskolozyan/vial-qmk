48bc646e3b97bf6950cdf45b88cdd4fd99fd8fda
Mels <melskolozyan@gmail.com>
2024-03-06 20:38:50 -0600

    Working luna and 10 layers

--- keyboards/boardsource/lulu/keymaps/vial/config.h
+++ keyboards/boardsource/lulu/keymaps/vial/config.h
@@ -5,7 +5,7 @@
 #define VIAL_KEYBOARD_UID {0xAB, 0x22, 0x68, 0x8B, 0x59, 0xD0, 0x81, 0x11}
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
 #define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
-
+#define DYNAMIC_KEYMAP_LAYER_COUNT 10
 #if defined(__AVR_ATmega32U4__)
     #undef ENABLE_RGB_MATRIX_BAND_SAT
     #undef ENABLE_RGB_MATRIX_BAND_VAL
